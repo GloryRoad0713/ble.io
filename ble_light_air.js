a5d4859e70e41e0897eceac97b1a449101206dd0
GloryRoad0713 <b1006090@gmail.com>
2018-12-18 13:11:15 +0900

    Update ble_light_air.js

--- ble_light_air.js
+++ ble_light_air.js
@@ -1,19 +1,18 @@
-var sound_links = [{link:"https://drive.google.com/uc?id=1UWVthywo-cLWRAafPUgCJDa4WnVMIPGw", name:"01"},
-                   {link:"https://drive.google.com/uc?id=11hceyP4BjvHF7cBBKHMfxPdRNtIenxG2", name:"02"},
-                   {link:"https://drive.google.com/uc?id=1-TN4fAtIb2KJkJkSeKwOT-rwcyWYsLqE", name:"03"},
-                   {link:"https://drive.google.com/uc?id=1l3yD9RbjGQaCrqpLxXKm6VKYwkdvawdZ", name:"04"},
-                   {link:"https://drive.google.com/uc?id=1-4aQLpD5XgxGBTOtKwtidITIkRpMJh2w", name:"05"},
-                   {link:"https://drive.google.com/uc?id=18LlOcMmZo2uJB_sxlsaALApnBcoFyjv9", name:"06"},
-                   {link:"https://drive.google.com/uc?id=1Lwwtjp8Ltjec4sS8qCZv66VxBq8oJSXO", name:"07"},
-                   {link:"https://drive.google.com/uc?id=1s6CBqsp3P_cqTkWA5TmYUWcQ_mfSHgOG", name:"08"}];
+var sound_links = [{link:"https://dl.dropboxusercontent.com/s/62b9cmkxx109nev/01_sound.mp3", name:"01"},
+                   {link:"https://dl.dropboxusercontent.com/s/s3fgpg3niutrljd/02_sound.mp3", name:"02"},
+                   {link:"https://dl.dropboxusercontent.com/s/j3quvvsrnqes32m/03_sound.mp3", name:"03"},
+                   {link:"https://dl.dropboxusercontent.com/s/yxrfy5kegcdug80/04_sound.mp3", name:"04"},
+                   {link:"https://dl.dropboxusercontent.com/s/e74cx8wjnbbsyb5/05_sound.mp3", name:"05"},
+                   {link:"https://dl.dropboxusercontent.com/s/0em3gqmewk91acx/06_sound.mp3", name:"06"},
+                   {link:"https://dl.dropboxusercontent.com/s/xjv58j2r600dlqr/07_sound.mp3", name:"07"},
+                   {link:"https://dl.dropboxusercontent.com/s/fi70pfrtqagbcf6/08_sound.mp3", name:"08"}];
+
+
 audio_list = new Array(8);
 for(var i=0; i < audio_list.length; i++){
     audio_list[i] = new Audio();
     audio_list[i].src = sound_links[i].link;
-
-    audio_list[i].play();
-    audio_list[i].currentTime = 0;
-    audio_list[i].pause();
+    audio_list[i].preload ="auto";
 
     //console.log(sound_links[i].link);
 }
@@ -32,22 +31,56 @@ var light_loop_state = false;
 var sound_loop_state = false;
 var pre_play_num = "0";
 
-document.body.addEventListener('click', function (event) {
-    ble_state = $("check_ble_connect").prop("checked");
-    console.log(ble_state);
-  //console.log(event.target.value);
-    if(event.target.id = "check_ble_connect" && ble_state == true){
-      connect();
-    }
-    else if(ble_state == false){
-      disconnect();
-    }
 
-    if(event.target.id = "send"){
-      sendMessage(event.target.value);
+$(function(){
+  $("#check_ble_connect").click(function(){
+    if (this.checked) {
+        connect();
+    } else {
+        disconnect();
+    }    
+  });
+  
+  
+  $("#check_light_loop").click(function(){
+    if (this.checked) {
+        light_loop_state = true;
+        console.log("light_loop_state is true");
+    } else {
+        light_loop_state = false;
+       console.log("light_loop_state is false");
     }
+  });
+  
+  $("#check_sound_loop").click(function(){
+    if (this.checked) {
+        sound_loop_state = true;
+        console.log("sound_loop_state is true");
+    } else {
+        sound_loop_state = false;
+        console.log("sound_loop_state is false");
+    }
+  });
+  
+  /*
+  $("#send").click(function(event){
+    sendMessage(event.target.value);
+    console.log("pressed button");
+  });
+  */
+});
 
-}, false)
+
+document.addEventListener("DOMContentLoaded", function(){
+  document.body.addEventListener('click', function (event) {
+      if(event.target.id = "send"){
+        console.log("pressed button");
+        sendMessage(event.target.value);
+      }
+  }, false);
+}, false);
+
+
 //chibi:bitに接続する
 function connect() {
   let options = {};
@@ -86,14 +119,22 @@ function connect() {
 function sendMessage(_num_str) {
   //console.log(_num_str);
   if (!bluetoothDevice || !bluetoothDevice.gatt.connected || !characteristic) return ;
-  light_loop_state = $("check_light_loop").prop("checked");
-  sound_loop_state = $("check_sound_loop").prop("checked");
-
+  //light_loop_state = $("check_light_loop").prop("checked");
+  //sound_loop_state = $("check_sound_loop").prop("checked");
+  
+  console.log(_num_str)
+  if (typeof _num_str === "undefined"){
+    console.log("_num_strは未定義")
+    return;
+  }
+    
   if(light_loop_state == true){
-    characteristic.writeValue(TextEncoder().encode("l1"));
+    characteristic.writeValue(new TextEncoder().encode("l1"));
+    console.log("send light loop signal")
   }
   else{
-    characteristic.writeValue(TextEncoder().encode("l0"));
+    characteristic.writeValue(new TextEncoder().encode("l0"));
+    console.log("send light not-loop signal")
   }
 
   var text = _num_str;
@@ -101,11 +142,12 @@ function sendMessage(_num_str) {
 
   setTimeout(function(){
     characteristic.writeValue(arrayBuffe);
-  },50);
+  },200);
 
   playSound(_num_str);
-
 }
+
+
 function playSound(_num_str){
   var c_name = "check" + _num_str;
   var o_name = "output" + _num_str;
@@ -120,7 +162,9 @@ function playSound(_num_str){
   else{
     audio_list[Number(_num_str)].loop = false;
   }
-
+  
+  console.log("c_state = " + c_state);
+  
   if(c_state == true){
     setTimeout(function(){
       audio_list[Number(_num_str)].pause();
@@ -128,7 +172,7 @@ function playSound(_num_str){
       audio_list[Number(_num_str)].play();
     },delay_time);
   }
-  console.log(delay_time);
+  console.log("delay_time = " + delay_time);
 
   pre_play_num = _num_str;
 

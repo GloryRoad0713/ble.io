0736670bc8b99df331315ccb79c95a1f7c2fb976
GloryRoad0713 <b1006090@gmail.com>
2020-01-08 16:50:55 +0900

    Add files via upload

---
+++ ble_light_cr5_02.js
@@ -0,0 +1,111 @@
+audio_list = new Array(11);
+//audio_duration = new Array(8);
+audio_count = new Array(11);
+for(var i=0; i < audio_list.length; i++){
+    audio_list[i] = new Audio();
+    audio_list[i].src = sound_links[i].link;
+    audio_list[i].preload ="auto";
+    audio_count[i] = 0;
+
+}
+
+//var audio_count = 0;
+
+var bluetoothDevice;
+var characteristic;
+//chibi:bit BLE UUID
+var LED_SERVICE_UUID                        = '000000ff-0000-1000-8000-00805f9b34fb';
+var LED_TEXT_CHARACTERISTIC_UUID            = '0000ff01-0000-1000-8000-00805f9b34fb';
+//ボタンイベントリスナー
+//d3.select("#connect").on("click", connect);
+//d3.select("#disconnect").on("click", disconnect);
+//d3.select("#send").on("click", sendMessage);
+var ble_state = false;
+var light_loop_state = false;
+var sound_loop_state = false;
+var pre_play_num = "0";
+
+var timerId;
+
+$(function(){
+  $("#check_ble_connect").click(function(){
+    if (this.checked) {
+        connect();
+    } else {
+        disconnect();
+    }
+  });
+
+  $("#base_light").click(function(){
+    changeBageLight();
+  });
+
+});
+
+//chibi:bitに接続する
+function connect() {
+  let options = {};
+
+  //options.acceptAllDevices = true;
+
+  options.filters = [
+    {services: [LED_SERVICE_UUID]},
+    {name: "ESP_GATTS_DEMO"}
+  ];
+
+  navigator.bluetooth.requestDevice(options)
+  .then(device => {
+    bluetoothDevice = device;
+    console.log("device", device);
+    return device.gatt.connect();
+  })
+  .then(server =>{
+    console.log("server", server)
+    return server.getPrimaryService(LED_SERVICE_UUID);
+  })
+  .then(service => {
+    console.log("service", service)
+    return service.getCharacteristic(LED_TEXT_CHARACTERISTIC_UUID)
+  })
+  .then(chara => {
+    console.log("characteristic", chara)
+    alert("BLE接続が完了しました。");
+    characteristic = chara;
+
+  })
+  .catch(error => {
+    console.log(error);
+  });
+
+
+}
+//ESP32に値を送信
+function sendInterruptLight(){
+  if (!bluetoothDevice || !bluetoothDevice.gatt.connected || !characteristic) return ;
+  var selected_base = $("input[name='radio1']:checked").val();
+  //characteristic.writeValue(new TextEncoder().encode(selected_base));
+  //console.log("selected_base = " + selected_base);
+
+  //光のループ処理関係
+  //var select_light_loop_num = $("#light_loop_num").val();
+  var select_light_loop_num = 1;
+
+  var text = "i" + selected_base + "," + select_light_loop_num;
+  var arrayBuffe = new TextEncoder().encode(text);
+
+  setTimeout(function(){
+    characteristic.writeValue(arrayBuffe);
+    console.log("send InterruptLight signal = " + text)
+  },200);
+}
+
+}
+
+
+
+//BEL切断処理
+function disconnect() {
+  if (!bluetoothDevice || !bluetoothDevice.gatt.connected) return ;
+  bluetoothDevice.gatt.disconnect();
+  alert("BLE接続を切断しました。")
+}
